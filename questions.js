9872c5e84dbc00dd6a3cbb2e6fa30089e58e1bdd
Tristan <tristan.warren2@finance.nsw.gov.au>
2019-03-28 22:42:33 +1100

    commit 2

--- questions.js
+++ questions.js
@@ -1,59 +1,48 @@
 alert("text");
 
-// let questions = [
-//   isUnder18,
-//   isOver18
-// ];
-
-let answer = prompt("How old are you?");
-
 let isUnder18 = {
-    text: "Are you under 18?",
-    answers: {
-      true: ["isOver18","isOver21","isOver60"],
-      false: ["isUnder16","isPrimaryStudent"]
-    }
+  text: "Are you under 18?",
+  answers: {
+    true: ["isOver18","isOver21","isOver60"],
+    false: ["isUnder16","isPrimaryStudent"]
+  }
 }
 
 let isOver18 = {
   text: "Are you over 18?",
-  ifTrue: ["text"]
+  answers: {
+    false: ["is_over_18","is_over_21","is_over_60"],
+    true: ["is_under_16","is_primary_student"]
+  }
 }
 
-function removeQuestion(qs, qName) {
-  let ind = qs.findIndex(qName)
-  qs.slice(ind, 1)
-};
+let isUnder16 = {
+  text: "Are you under 16?",
+  answers: {
+    false: [],
+    true: ["is_over_18","is_over_21"]
+  }
+}
+testList = ["car", "boat", "train", "plain"]
+testNames = ["train", "boat"]
+
+console.log("First Console --- " + testList)
+
+function removeQuestions(qList, qNames) {
+  qNames.forEach(qName => {
+    console.log(qList.slice(qList.indexOf(qName), 1))
+    // qList = qList.slice(qList.indexOf(qName), 1)
+  });
+}
+
+removeQuestions(testList, testNames)
+
+console.log("Second Console --- " + testList)
 
 let questions = [
   isUnder18,
-  isOver18
+  isOver18,
+  isUnder16
 ];
 
-
-// {
-//     "is_under_18": {
-//         "text": "Are you under 18?",
-//         "format": "date",
-//         "answers": {
-//           "true": ["is_over_18","is_over_21","is_over_60"],
-//           "false": ["is_under_16","is_primary_student"]
-//         }
-//     },
-//     "is_over_18": {
-//         "text": "Are you over 18?",
-//         "format": "date",
-//         "answers": {
-//           "false": ["is_over_18","is_over_21","is_over_60"],
-//           "true": ["is_under_16","is_primary_student"]
-//         }
-//     },
-//     "is_under_16": {
-//         "text": "Are you under 16?",
-//         "format": "date",
-//         "answers": {
-//           "false": [],
-//           "true": ["is_over_18","is_over_21"]
-//         }
-//     }
-//   }
+let answer = prompt(isUnder18.text);
